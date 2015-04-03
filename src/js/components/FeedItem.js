4ee9fe2c57191e9d40583274ce6cfb5bb4c767d2
dviramontes <dviramontes@gmail.com>
2015-04-03 08:21:16 -0600

    updates styles

--- src/js/components/FeedItem.js
+++ src/js/components/FeedItem.js
@@ -4,23 +4,21 @@ var FeedItem = React.createClass({
 	render: function () {
 		return (
 			<li key={this.props.id}>
-				<div className="container-fluid">
+				<div className="container">
 					<div className="row">
 						<div className="col-sm-12 col-md-12">
 							<div className="thumbnail">
-								<img className="effect image-responsive" src={this.props.imgurl}/>
-								<div className="caption">
-									<h3>
-										<a href={this.props.link}>{this.props.title}</a>
-									</h3>
-									<span className="text-center">
-										<button className="btn btn-success" type="button">
-											votes
-											<span className="badge">{this.props.voteCount}</span>
-										</button>
-									</span>
-								</div>
+								<img className="effect" src={this.props.imgurl}/>
 							</div>
+						</div>
+						<div className="caption">
+							<h3>
+								<a href={this.props.link}>{this.props.title}</a>
+							</h3>
+							<button className="btn btn-success" type="button">
+								votes
+								<span className="badge">{this.props.voteCount}</span>
+							</button>
 						</div>
 					</div>
 				</div>
